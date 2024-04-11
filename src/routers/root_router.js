e086c0523a9e97baf3030d34ff30855d2f82d5b3
taeyoung <xodud5080@naver.com>
2024-04-11 15:35:53 +0900

    test

--- src/routers/root_router.js
+++ src/routers/root_router.js
@@ -6,6 +6,10 @@ module.exports = (app) => {
   const loginRouter = require("./login/login_router")(app);
   app.use("/login", loginRouter);
 
+  // register 부분 router 정의
+  const registerRouter = require("./login/register_router")(app);
+  app.use("/register", loginRouter);
+
   // root경로의 router
   const router = require("express").Router();
   router.get("/", (req, res) => {
