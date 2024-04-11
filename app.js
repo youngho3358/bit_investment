74a939b5e3acd026a80dcb8fb64fdee81c3058b0
youngho3358 <youngho3358@gmail.com>
2024-04-11 14:14:22 +0900

    카카오 로그인 버튼 생성

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.use(bodyParser.urlencoded());
 const router = require("./src/routers/root_router")(app);
 app.use("/", router);
 
+
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
--- src/controller/login/login_controller.js
+++ src/controller/login/login_controller.js
@@ -1,9 +1,22 @@
 const service = require("../../service/login/login_service")
+const fs = require("fs");
+const path = require("path");
 
 const views = {
     login_input : (req, res) => {
-        res.render("./login/login_input");
-    }
+        // 로고 이미지 사용
+        const logoPath = "../../../img/logo/upbit.png";
+        const logoBase64 = fs.readFileSync(path.join(__dirname, logoPath), 'base64');
+        const logoDataURI = `data:image/jpeg;base64,${logoBase64}`;
+
+        // 카카오 로그인 이미지 사용
+        const kakao_loginPath = "../../../img/login/kakao_login.png";
+        const kakao_loginBase64 = fs.readFileSync(path.join(__dirname, kakao_loginPath), 'base64');
+        const kakao_loginDataURI = `data:image/jpeg;base64,${kakao_loginBase64}`;
+
+        res.render("./login/login_input", {logoDataURI : logoDataURI, kakao_loginDataURI : kakao_loginDataURI});
+    },
+
 }
 
 const process = {
