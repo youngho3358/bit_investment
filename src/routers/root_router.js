956afc9d6c943f9c3f7a7cf00e591a6be77c782f
minseok <heekms6@naver.com>
2024-04-11 09:39:27 +0900

    게시판_글쓰기_01

--- src/routers/root_router.js
+++ src/routers/root_router.js
@@ -2,6 +2,10 @@ module.exports = (app) => {
   
   // 각자 사용할 router 정의
 
+  //게시판 글쓰기 부분 라우터
+  const boardwriteRouter = require("./board_write/BW_router")
+  app.use("/boardwrite", boardwriteRouter)
+
   // login 부분 router 정의
   const loginRouter = require("./login/login_router")(app);
   app.use("/login", loginRouter);
