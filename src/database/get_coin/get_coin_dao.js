6bbb04026a6a2e19dffbe51a6ffc1617b04f08c0
taeyoung <xodud5080@naver.com>
2024-04-23 17:05:45 +0900

    거래소 뭔가 문제 발생

--- src/database/get_coin/get_coin_dao.js
+++ src/database/get_coin/get_coin_dao.js
@@ -5,11 +5,19 @@ oracledb.autoCommit = true;
 
 const insert = {    
     btc: async (coin) => {
+        // console.log("coin : ", coin);
+        for(let i = 0; i < coin.length; i++){
+            if (coin[i] == "") {
+                console.log("aaaaaaaaaaaaaaaa");
+            }else if(coin[i] == undefined){
+                console.log("bbbbbbbbbbbbbbbbbb");
+            }
+        }
         const con = await oracledb.getConnection(dbConfig);
-        console.log("길이 : ", coin.length);
-        for(let i = (coin.length - 1); i >= 0; i--){
-            await con.execute(`insert into coin_info(coin_name, coin_eng_name, high_price, low_price, now_price, change_rate, change_price, trans_price, trans_volume, coin_date) 
-                                             values('${coin[i].name}', '${coin[i].eng_name}', ${coin[i].high_price}, ${coin[i].low_price}, ${coin[i].now_price}, '${coin[i].change_rate}', ${coin[i].change_price}, ${coin[i].trans_price}, ${coin[i].trans_volume}, sysdate + 9/24)`);
+        //console.log("길이 : ", coin.length);
+        for(let i = 0; i < coin.length; i++){
+            await con.execute(`insert into coin_info(coin_name, coin_eng_name, high_price, low_price, now_price, closing_price, change_rate, change_price, trans_price, trans_volume, coin_date) 
+                                             values('${coin[i].name}', '${coin[i].eng_name}', ${coin[i].high_price}, ${coin[i].low_price}, ${coin[i].now_price}, ${coin[i].closing_price},'${coin[i].change_rate}', ${coin[i].change_price}, ${coin[i].trans_price}, ${coin[i].trans_volume}, systimestamp + 9/24)`);
         }
     },
 }
--- src/database/market/market_dao.js
+++ src/database/market/market_dao.js
@@ -8,10 +8,47 @@ const views = {
         let data1 = await con.execute(`select * from (select * from coin_info ORDER BY coin_date DESC, trans_price DESC) where rownum=1`);
         let data2 = await con.execute(`select * from coin_info where (coin_date, trans_price) = (select coin_date, trans_price from (select coin_date, trans_price, ROW_NUMBER() OVER (ORDER BY coin_date DESC, trans_price DESC) AS row_num from coin_info) where row_num = 2)`);
         let data3 = await con.execute(`select * from coin_info where (coin_date, trans_price) = (select coin_date, trans_price from (select coin_date, trans_price, ROW_NUMBER() OVER (ORDER BY coin_date DESC, trans_price DESC) AS row_num from coin_info) where row_num = 3)`);
+        let data4 = await con.execute(`select * from coin_info where (coin_date, trans_price) = (select coin_date, trans_price from (select coin_date, trans_price, ROW_NUMBER() OVER (ORDER BY coin_date DESC, trans_price DESC) AS row_num from coin_info) where row_num = 4)`);
+        let data5 = await con.execute(`select * from coin_info where (coin_date, trans_price) = (select coin_date, trans_price from (select coin_date, trans_price, ROW_NUMBER() OVER (ORDER BY coin_date DESC, trans_price DESC) AS row_num from coin_info) where row_num = 5)`);
+        let data6 = await con.execute(`select * from coin_info where (coin_date, trans_price) = (select coin_date, trans_price from (select coin_date, trans_price, ROW_NUMBER() OVER (ORDER BY coin_date DESC, trans_price DESC) AS row_num from coin_info) where row_num = 6)`);
+        let data7 = await con.execute(`select * from coin_info where (coin_date, trans_price) = (select coin_date, trans_price from (select coin_date, trans_price, ROW_NUMBER() OVER (ORDER BY coin_date DESC, trans_price DESC) AS row_num from coin_info) where row_num = 7)`);
+        let data8 = await con.execute(`select * from coin_info where (coin_date, trans_price) = (select coin_date, trans_price from (select coin_date, trans_price, ROW_NUMBER() OVER (ORDER BY coin_date DESC, trans_price DESC) AS row_num from coin_info) where row_num = 8)`);
+        let data9 = await con.execute(`select * from coin_info where (coin_date, trans_price) = (select coin_date, trans_price from (select coin_date, trans_price, ROW_NUMBER() OVER (ORDER BY coin_date DESC, trans_price DESC) AS row_num from coin_info) where row_num = 9)`);
+        let data10 = await con.execute(`select * from coin_info where (coin_date, trans_price) = (select coin_date, trans_price from (select coin_date, trans_price, ROW_NUMBER() OVER (ORDER BY coin_date DESC, trans_price DESC) AS row_num from coin_info) where row_num = 10)`);
+        let data11 = await con.execute(`select * from coin_info where (coin_date, trans_price) = (select coin_date, trans_price from (select coin_date, trans_price, ROW_NUMBER() OVER (ORDER BY coin_date DESC, trans_price DESC) AS row_num from coin_info) where row_num = 11)`);
+        let data12 = await con.execute(`select * from coin_info where (coin_date, trans_price) = (select coin_date, trans_price from (select coin_date, trans_price, ROW_NUMBER() OVER (ORDER BY coin_date DESC, trans_price DESC) AS row_num from coin_info) where row_num = 12)`);
+        let data13 = await con.execute(`select * from coin_info where (coin_date, trans_price) = (select coin_date, trans_price from (select coin_date, trans_price, ROW_NUMBER() OVER (ORDER BY coin_date DESC, trans_price DESC) AS row_num from coin_info) where row_num = 13)`);
+        let data14 = await con.execute(`select * from coin_info where (coin_date, trans_price) = (select coin_date, trans_price from (select coin_date, trans_price, ROW_NUMBER() OVER (ORDER BY coin_date DESC, trans_price DESC) AS row_num from coin_info) where row_num = 14)`);
+        let data15 = await con.execute(`select * from coin_info where (coin_date, trans_price) = (select coin_date, trans_price from (select coin_date, trans_price, ROW_NUMBER() OVER (ORDER BY coin_date DESC, trans_price DESC) AS row_num from coin_info) where row_num = 15)`);
+        let data16 = await con.execute(`select * from coin_info where (coin_date, trans_price) = (select coin_date, trans_price from (select coin_date, trans_price, ROW_NUMBER() OVER (ORDER BY coin_date DESC, trans_price DESC) AS row_num from coin_info) where row_num = 16)`);
+        let data17 = await con.execute(`select * from coin_info where (coin_date, trans_price) = (select coin_date, trans_price from (select coin_date, trans_price, ROW_NUMBER() OVER (ORDER BY coin_date DESC, trans_price DESC) AS row_num from coin_info) where row_num = 17)`);
+        let data18 = await con.execute(`select * from coin_info where (coin_date, trans_price) = (select coin_date, trans_price from (select coin_date, trans_price, ROW_NUMBER() OVER (ORDER BY coin_date DESC, trans_price DESC) AS row_num from coin_info) where row_num = 18)`);
+        let data19 = await con.execute(`select * from coin_info where (coin_date, trans_price) = (select coin_date, trans_price from (select coin_date, trans_price, ROW_NUMBER() OVER (ORDER BY coin_date DESC, trans_price DESC) AS row_num from coin_info) where row_num = 19)`);
+        let data20 = await con.execute(`select * from coin_info where (coin_date, trans_price) = (select coin_date, trans_price from (select coin_date, trans_price, ROW_NUMBER() OVER (ORDER BY coin_date DESC, trans_price DESC) AS row_num from coin_info) where row_num = 20)`);
+
         let data = [];
+
         data.push( data1.rows[0]);
         data.push( data2.rows[0]);
         data.push( data3.rows[0]);
+        data.push( data4.rows[0]);
+        data.push( data5.rows[0]);
+        data.push( data6.rows[0]);
+        data.push( data7.rows[0]);
+        data.push( data8.rows[0]);
+        data.push( data9.rows[0]);
+        data.push( data10.rows[0]);
+        data.push( data11.rows[0]);
+        data.push( data12.rows[0]);
+        data.push( data13.rows[0]);
+        data.push( data14.rows[0]);
+        data.push( data15.rows[0]);
+        data.push( data16.rows[0]);
+        data.push( data17.rows[0]);
+        data.push( data18.rows[0]);
+        data.push( data19.rows[0]);
+        data.push( data20.rows[0]);
+
         return data;
     }
 }
