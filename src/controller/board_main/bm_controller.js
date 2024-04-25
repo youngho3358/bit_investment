6f115bcfb8fe29024f72d5395acfa3b5cdf162fb
오승민 <cjdvhen@naver.com>
2024-04-25 17:55:15 +0900

    수정

--- src/controller/board_main/bm_controller.js
+++ src/controller/board_main/bm_controller.js
@@ -11,9 +11,10 @@ const logoDataURI = `data:image/jpeg;base64,${logoBase64}`;
 
 const bm_input = async (req, res) => {
     const data = await ser.boardRead.list(req.query.start);
-
+    let member = req.session.member;
     res.render("board/bm_input",{
         data,
+        member,
         category_id : 4,
         list : data.list,
         start : data.start,
@@ -24,13 +25,14 @@ const bm_input = async (req, res) => {
 
 const board_views = {
     data : async (req,res) => {
-
+        let member = req.session.member;
         const data = await ser.boardRead.data(req.params.num)
         const cmtdata = await ser.boardRead.cmtdata(req.params.num)
         // const username = req.session.username;
 
         res.render("board/data",{
             data,
+            member,
             // username,       
             list : data.list,
             start : data.start,
@@ -40,6 +42,7 @@ const board_views = {
         })
     },
     category_id : async (req,res) =>{
+        let member = req.session.member;
         let category_id = req.params.category_id;
         let list;
         if(category_id == 0 ){
@@ -58,6 +61,7 @@ const board_views = {
 
         res.render("board/bm_input", {
             start : 1,
+            member,
             page,
             logoDataURI, 
             list,
@@ -76,15 +80,16 @@ const board_views = {
     search : async (req, res) => {
             const keyword = req.query.keyword;
             const results = await ser.searchPosts(keyword);
-
-            res.render('board/search', { results });
+            let member = req.session.member;
+            res.render('board/search', { results,member });
     },
     getPost: async (req, res) => {
         const BOARD_ID = req.params.BOARD_ID;
+        let member = req.session.member;
         try {
             await ser.incrementViews(BOARD_ID);
-            const post = await ser.getPost(BOARD_ID); // 게시물 데이터 가져오기
-            res.render("data", { post }); // 게시물 데이터를 전달하여 data.ejs를 렌더링
+            const host = await ser.getPost(BOARD_ID); // 게시물 데이터 가져오기
+            res.render("data",host,member); // 게시물 데이터를 전달하여 data.ejs를 렌더링
         } catch (error) {
             console.log("조회수 증가 오류", error);
             res.status(500).send("조회수 증가 중 오류가 발생했습니다.");
